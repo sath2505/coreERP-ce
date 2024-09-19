d3e468baf047805a3ecd8fd93c53e33283668bc1
sath2505 <sathwikshetty025@gmail.com>
2024-09-19 12:08:18 +0530

    Fixed Bugs TDS-PersonType,Rate,Section

--- core/tds/personType/PersonTypeValidator.php
+++ core/tds/personType/PersonTypeValidator.php
@@ -1,13 +1,6 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace app\core\tds\personType;
-
 
 /**
  * Description of PersonTypeValidator
@@ -15,25 +8,30 @@ namespace app\core\tds\personType;
  * @author Shrishail
  */
 class PersonTypeValidator extends \app\cwf\vsla\xmlbo\ValidatorBase {
-    
-    public function validatePersonTypeEditForm() {
-        // conduct default form validations
+
+    public function validatePersonTypeEditForm(): void {
+ 
         $formView = \app\cwf\vsla\xml\CwfXmlLoader::loadFile($this->modulePath, $this->formName);
+        if (!$formView) {
+            throw new \Exception('Form view could not be loaded.');
+        }
         $this->validateUsingForm($this->bo, $formView);
-        
-        // conduct business rule validations
+ 
         $this->validateBusinessRules();
-     }
-    
-    private function validateBusinessRules() {        
-        
-        // Validate duplicate TDS Person Type
+    }
+ 
+    private function validateBusinessRules(): void {
+     
+        if (empty($this->bo->person_type_desc)) {
+            $this->bo->addBRule('Person Type description is required.');
+        }
+ 
         $cmm = new \app\cwf\vsla\data\SqlCommand();
-        $cmm->setCommandText('select person_type_desc from tds.person_type where person_type_desc ilike :pperson_type_desc and person_type_id!=:pperson_type_id');
+        $cmm->setCommandText('SELECT person_type_desc FROM tds.person_type WHERE person_type_desc ILIKE :pperson_type_desc AND person_type_id != :pperson_type_id');
         $cmm->addParam('pperson_type_desc', $this->bo->person_type_desc);
         $cmm->addParam('pperson_type_id', $this->bo->person_type_id);
         $result = \app\cwf\vsla\data\DataConnect::getData($cmm);
-        if(count($result->Rows())>0) {
+        if (count($result->Rows()) > 0) {
             $this->bo->addBRule('Person Type already exists. Duplicate Person Type not allowed.');
         }
     }
